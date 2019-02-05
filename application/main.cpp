a2314b692e142cf757b7dc4727c7cbe0ead5fa95
FJAConceicao <32593546+FJAConceicao@users.noreply.github.com>
2019-02-05 01:32:52 -0200

    Update main.cpp

--- application/main.cpp
+++ application/main.cpp
@@ -1,13 +1,6 @@
 #include "mainwindow.h"
 #include <QApplication>
 
-#include <QDebug>
-#include <Qt>
-
-#include <iostream>
-
-#include "student.h"
-
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
