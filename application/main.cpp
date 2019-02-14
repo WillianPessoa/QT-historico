eec94ef16d2df12f876b90b443abe9a0b8014df5
Willian Pessoa <willian.pessoa@petrosoftdesign.com>
2019-02-14 16:31:23 -0200

    [TASK] Main realizando testes de leitura

--- application/main.cpp
+++ application/main.cpp
@@ -1,11 +1,21 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+#include "importer.h"
+
 int main(int argc, char *argv[])
 {
+    Q_UNUSED(argc)
+    Q_UNUSED(argv)
+
+    /** Ignorando parte visual
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    return a.exec(); */
 
-    return a.exec();
+    // TODO: Implementar testes
+    // Realiza a leitura dos arquivos de teste
+    QList<IndividualSheet> list = Importer::importStudentsFrom(":/samples");
+    return 0;
 }
