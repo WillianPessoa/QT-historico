be09b83f21a1952da4f5b97303c37b98e388f623
FJAConceicao <lipeconceicao13@gmail.com>
2019-02-21 18:09:22 -0300

    [TASK] Gravar dados do aluno no modelo de historico

--- application/exporter.cpp
+++ application/exporter.cpp
@@ -1,5 +1,7 @@
 #include "exporter.h"
 
+#include "xlsxdocument.h"
+
 Exporter::Exporter()
 {
 
@@ -12,5 +14,18 @@ void Exporter::exportHistory(const QList<Student> &students)
 
 void Exporter::exportHistory(const QList<Student> &students, const QDir &exportHistoryDir)
 {
+    for(const Student &student : students){
 
+        //Criar documento do modelo de histórico
+        QXlsx::Document historic("modelHistoric.xlsx");
+
+        //Gravar dados pessoais
+        historic.write("J17", student.name());       //name
+        historic.write("L19", student.dateOfBirth().toString("dd/MM/yyyy"));    //data de nascimento
+
+        //Gravar notas do 1º, 2º e 3º anos
+
+        //salvar histórico
+        //historic.saveAs();
+    }
 }
