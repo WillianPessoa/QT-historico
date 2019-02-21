70c68d777334550ac2761a134676a521243b0933
Mari Ferreira <mari-191938@hotmail.com>
2019-02-21 20:02:42 -0300

    add dados pessoais

--- application/exporter.cpp
+++ application/exporter.cpp
@@ -9,19 +9,28 @@ Exporter::Exporter()
 
 void Exporter::exportHistory(const QList<Student> &students)
 {
-    for(const Student &student : students){
 
-            //Criar documento do modelo de histórico
-            QXlsx::Document historic("modelHistoric.xlsx");
-
-            //Gravar notas do 1º, 2º e 3º anos
-
-            //salvar histórico
-            historic.saveAs("./historical/" + student.name());
-        }
 }
 
 void Exporter::exportHistory(const QList<Student> &students, const QDir &exportHistoryDir)
 {
+    for(const Student &student : students){
 
+        //Criar documento do modelo de histórico
+        QXlsx::Document historic("modelHistoric.xlsx");
+
+        //Gravar dados pessoais
+        historic.write("J17", student.name());       //name
+        historic.write("L19", student.dateOfBirth().toString("dd/MM/yyyy"));    //data de nascimento
+        historic.write("G21", student.fatherName());         //pai
+        historic.write("AD21", student.motherName());        //mae
+        historic.write("I23", student.IDNumber());           //rg
+        historic.write("AA23", student.IDIssuingInstitution());   //orgão expeditor
+        historic.write("AP23", student.IDIssueDate());    //data de emissao
+
+        //Gravar notas do 1º, 2º e 3º anos
+
+        //salvar histórico
+        //historic.saveAs();
+    }
 }
